a86c9b1cf3f951297c49a7d4aca911b6fedd0c0c
jeonghunkim942 <57852981+jeonghunkim942@users.noreply.github.com>
2019-11-17 13:23:42 +0900

    first version

---
+++ parameter.php
@@ -0,0 +1,12 @@
+<!doctype hmtl>
+<html>
+  <head>
+    <meta charset = "utf-8">
+  </head>
+
+  <body>
+    안녕하세요.
+    <?php echo $_GET['add'];  ?> 에 사는
+    <?php echo $_GET['name'];  ?> 님
+  </body>
+</html>
